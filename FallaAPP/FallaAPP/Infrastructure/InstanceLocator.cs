85296a7fe06849153da86ae5199858873ecd466c
Antonio <alorenzoesparza@gmail.com>
2018-05-21 19:14:17 +0200

    Login

---
+++ FallaAPP/FallaAPP/Infrastructure/InstanceLocator.cs
@@ -0,0 +1,19 @@
+﻿using FallaAPP.ViewModels;
+
+namespace FallaAPP.Infrastructure
+{
+    public class InstanceLocator
+    {
+        #region Propiedades
+        public MainViewModel Main { get; set; }
+
+        #endregion
+
+        #region Constructores
+        public InstanceLocator()
+        {
+            this.Main = new MainViewModel();
+        }
+        #endregion
+    }
+}
